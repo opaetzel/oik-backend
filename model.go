ef07fcc16d70971197fcada3c173e294df19fe2c
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-08-08 08:05:35 +0200

    add json struct tags to model.go

--- model.go
+++ model.go
@@ -2,47 +2,47 @@ package main
 
 type Image struct {
 	path    string
-	Caption string
-	Credits string
-	ID      int
+	Caption string `json:"caption"`
+	Credits string `json:"credits"`
+	ID      int    `json:"id"`
 }
 
 type RotateImage struct {
 	basepath string
 	num      int
-	Caption  string
-	Credits  string
-	ID       int
+	Caption  string `json:"caption"`
+	Credits  string `json:"credits"`
+	ID       int    `json:"id"`
 }
 
 type Text struct {
-	Markdown string
-	Html     string
-	ID       int
+	Markdown string `json:"markdown"`
+	Html     string `json:"html"`
+	ID       int    `json:"id"`
 }
 
 type Column struct {
-	Image *Image
-	Text  *Text
-	ID    int
+	Image *Image `json:"image"`
+	Text  *Text  `json:"text"`
+	ID    int    `json:"id"`
 }
 
 type Row struct {
-	Left  Column
-	Right Column
-	ID    int
+	Left  Column `json:"left"`
+	Right Column `json:"right"`
+	ID    int    `json:"id"`
 }
 
 type Page struct {
-	Title string
-	Rows  []Row
-	ID    int
+	Title string `json:"title"`
+	Rows  []Row  `json:"rows"`
+	ID    int    `json:"id"`
 }
 
 type Unit struct {
-	Title     string
-	PageIds   []string
-	Published bool
-	UserId    string
-	ID        int
+	Title     string   `json:"title"`
+	PageIds   []string `json:"pageids"`
+	Published bool     `json:"published"`
+	UserId    string   `json:"userid"`
+	ID        int      `json:"id"`
 }
