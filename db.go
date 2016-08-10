a0d501dbcc19cd152b1f6721bfc4c59920d39dcf
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-08-10 08:18:45 +0200

    simplify database schema

--- db.go
+++ db.go
@@ -27,22 +27,12 @@ CREATE TABLE IF NOT EXISTS pages (
 );
 
 CREATE TABLE IF NOT EXISTS rows (
-	left_column_id integer,
-	right_column_id integer,
+	left_markdown text,
+	left_html text,
+	right_markdown text,
+	right_html text,
 	page_id integer,
 	row_id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS columns (
-	image_id integer,
-	text_id integer,
-	column_id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS texts (
-	markdown text,
-	html text,
-	text_id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS rotate_images ( 
@@ -101,12 +91,9 @@ func GetAllUnits() ([]Unit, error) {
 
 func GetPageById(id int) (*Page, error) {
 	query := `
-		SELECT pages.*, json_agg(rows.*) AS row, json_agg(columns.*) AS column, json_agg(images.*) AS image, json_agg(texts.*) AS text FROM pages 
+		SELECT pages.*, json_agg(rows.*) AS row, FROM pages 
 			LEFT JOIN rows ON rows.page_id = pages.page_id
-			LEFT JOIN columns ON columns.column_id = rows.left_column_id OR columns.column_id = rows.right_column_id
-			LEFT JOIN texts ON texts.text_id = columns.text_id
-			LEFT JOIN images ON images.image_id = columns.image_id
-			GROUP BY pages.page_id, rows.row_id, columns.row_id
+			GROUP BY pages.page_id
 		WHERE pages.page_id=$1;
 		`
 	_, err := db.Query(query, id)
